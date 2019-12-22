252e7368b416f53a5b745c4dca1bd027fcbc8379
kdineshreddy009 <kdineshreddy009@gmail.com>
2019-12-23 01:07:28 +0530

    edits

--- server.js
+++ server.js
@@ -16,14 +16,11 @@ app.use(express.static(__dirname + '/Public', {
     maxAge: 600000
 }));
 
-
-//Use body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', router);
 
 router.get("/", function(req, res) {
-    console.log("in route/");
     if (req.cookies.pinkBlueUser === "Store Manager") {
         res.redirect("/storemanager_view/logged_user");
     } else if (req.cookies.pinkBlueUser === "Store Assistant") {
@@ -41,23 +38,6 @@ router.get("/login/:credentials", (req, res) => {
         // res.send(useris);
         res.send({ user: useris });
     });
-});
-
-router.get("/staff_view/:user", function(req, res) {
-    if (req.cookies.pinkBlueUser === "Store Assistant") {
-        res.sendFile(path.join(__dirname, './Public', 'staff.html'));
-    } else {
-        throw new Error('wrong access to staff_view URL');
-    }
-});
-
-
-router.get("/storemanager_view/:user", function(req, res) {
-    if (req.cookies.pinkBlueUser === "Store Manager") {
-        res.sendFile(path.join(__dirname, './Public', 'storemanager.html')); //res.render('abc'); also works with pug
-    } else {
-        throw new Error('wrong access to storemanager_view URL');
-    }
 });
 
 
@@ -102,7 +82,26 @@ router.get("/sendForApproval/:productId", function(req, res) {
             });
         }
     } catch (err) {
+        next(err);
+    }
+});
 
+//redirects
+
+router.get("/staff_view/:user", function(req, res) {
+    if (req.cookies.pinkBlueUser === "Store Assistant") {
+        res.sendFile(path.join(__dirname, './Public', 'staff.html'));
+    } else {
+        throw new Error('wrong access to staff_view URL');
+    }
+});
+
+
+router.get("/storemanager_view/:user", function(req, res) {
+    if (req.cookies.pinkBlueUser === "Store Manager") {
+        res.sendFile(path.join(__dirname, './Public', 'storemanager.html')); //res.render('abc'); also works with pug
+    } else {
+        throw new Error('wrong access to storemanager_view URL');
     }
 });
 
@@ -198,9 +197,9 @@ app.get(["/ping", "/sping"], function(req, res) {
 
 app.use(function(err, req, res, next) {
     console.log("Entered default error handler of express")
-    console.error(err.message); // Logs error message in our server's console
+    console.error(err); // Logs error message in our server's console
     if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-    res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+    res.status(err.statusCode).send(err); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
 
 const host = process.env.NODE_ENV === 'production' ? 'localhost' : '0.0.0.0';
